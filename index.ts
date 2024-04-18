a9721d88cc8b06aedbb835343ab9136e773270d7
Ahmed Ali Kanhar <160313971+ahmedkanhar@users.noreply.github.com>
2024-04-18 18:55:46 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const answers = await inquirer.prompt([
     {
         name : "userGuessedNumber",
         type : "number",
-        message : "Please guess a number between 1 - 10",
+        message : "Please guess a number between 1 - 6",
     },
 ]);
 
